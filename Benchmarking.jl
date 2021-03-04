5d474c9c20b26e4bd108f27b92d1782a3137ee14
JoelTrent <joeltrent1@gmail.com>
2021-03-04 15:52:30 +1300

    Module used to import Julia function from one file to another.

--- Benchmarking.jl
+++ Benchmarking.jl
@@ -17,37 +17,87 @@ using Conda
 using BenchmarkTools
 using PyCall
 
-# Julia version of sumArray
-function jlSumArray(a)
-    arraySum = 0.0
-    for i in 1:length(a)
-        arraySum += a[i]
-    end
-    return sum
+
+# Benchmarking Python vs Julia
+if true
+
+    # Loading a Julia function (must be within a module). Set path to current
+    # CHANGE WHEN 'MAIN' BRANCH UPDATED
+    push!( LOAD_PATH, "./" )
+    using moduleTest # found this better than doing 'include'
+
+    # create local function wrapper
+    juliaGillespieDirect = moduleTest.gillespieDirect2Processes
+
+    # import python function. Uncomment when ready
+    #py"""
+    #from ???? import ???
+    #"""
+
+    # make local function wrapper for benchmark
+    # pythonGillespieDirect = py"???"
+
+    # Setup the inputs for the test
+    N = [5, 10, 50, 100,1000,10000,100000]
+
+    S_total = N .- 1
+    I_total = N .* 0 .+ 1
+    R_total = N .* 0
+
+    t_max = 200
+    alpha = 0.4
+    gamma = 0.0004
+
+    i = 7
+
+    # setup a loop that benchmarks each function for different N values
+
+    # pyGillespieMark = @benchmark pythonGillespieDirect(t_max, S_total[i], I_total[i], R_total[i], alpha, gamma, N[i])
+
+    jlGillespieMark = @benchmark juliaGillespieDirect(t_max, S_total[i], I_total[i], R_total[i], alpha, gamma, N[i])
+
+    # graph the benchmark of time as N increases. Recommend two graphs next to
+    # each other with median on one and mean on other.
+
 end
 
 
-# Create a reasonably large array to compute the sum of
-a = rand(10^7)
 
-# Py looks in current directory, may be unneccesary
-pushfirst!(PyVector(pyimport("sys")."path"), "")
+# Test benchmark on summing an array
+if false
 
-# py code break to import the function desired
-py"""
-from BenchmarkTest import sumArray
-"""
+    # Julia version of sumArray
+    function jlSumArray(a)
+        arraySum = 0.0
+        for i in 1:length(a)
+            arraySum += a[i]
+        end
+        return sum
+    end
 
-# make locally accessible for benchmark
-pySumArray = py"sumArray"
+    # Create a reasonably large array to compute the sum of
+    a = rand(10^7)
 
-# using the function
-pySumArray(a)
+    # Py looks in current directory, may be unneccesary
+    pushfirst!(PyVector(pyimport("sys")."path"), "")
 
-pySumMark = @benchmark pySumArray(a)
+    # py code break to import the function desired
+    py"""
+    from BenchmarkTest import sumArray
+    """
 
-jlSumMark = @benchmark jlSumArray(a)
+    # make local function wrapper for benchmark
+    pySumArray = py"sumArray"
 
-numpySum = pyimport("numpy")."sum"
+    # using the function
+    pySumArray(a)
 
-pyNumpySumMark = @benchmark numpySum(a)
+    pySumMark = @benchmark pySumArray(a)
+
+    jlSumMark = @benchmark jlSumArray(a)
+
+    numpySum = pyimport("numpy")."sum"
+
+    pyNumpySumMark = @benchmark numpySum(a)
+
+end

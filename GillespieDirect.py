4afff389801c334977fdd86cf7d8f4838bd3f41d
nesaking100 <josh@looker.co.nz>
2021-03-04 19:10:37 +1300

    add gitignore and simulation using modules, py version init

---
+++ GillespieDirect.py
@@ -0,0 +1,127 @@
+'''
+This is a code base for a simple SIR simulation using the Gillespie Direct
+Method
+
+Author: Joel Trent and Josh Looker
+'''
+
+
+def gillespieDirect2Processes(t_max, S_total, I_total, R_total, alpha, gamma, N, t_init=0):
+    '''
+    Inputs
+    t_init  : Initial time (default of 0)
+    t_max   : Simulation end time
+    S_total : Num people susceptible to infection
+    I_total : Num people infected
+    R_total : Num people recovered
+    N       : Population size
+    alpha   : probability of infected person recovering [0,1]
+    gamma   : probability of susceptible person being infected [0,1]
+
+    Outputs
+    t       : Array of times at which events have occured
+    S       : Array of Num people susceptible at each t
+    I       : Array of Num people infected at each t
+    R       : Array of Num people recovered at each t
+    '''
+
+
+    # initialise outputs
+    t = [copy(t_init)]
+    S = [copy(S_total)]
+    I = [copy(I_total)]
+    R = [copy(R_total)]
+
+    while t[end] < t_max && I_total != 0
+        # calculate the propensities to transition
+        # h1 is propensity for infection, h2 is propensity for recovery
+        h_i = [gamma * I_total * S_total, alpha * I_total]
+        h = sum(h_i)
+
+        # time to any event occurring
+        delta_t = -log(1-rand())/h
+        #println(delta_t)
+
+        # selection probabilities for each transition process. sum(j) = 1
+        j = h_i ./ h
+
+        # coding this way so can include more processes later with additional
+        # elseif
+        # could be done more efficiently if large number of processes
+        choice = rand()
+
+        if choice < j[1] && S_total != 0  # (S->I)
+            S_total -= 1
+            I_total += 1
+        else    # (I->R) (assumes that I is not 0)
+            I_total -= 1
+            R_total += 1
+        end
+
+        push!(t, t[end] + delta_t)
+        push!(S, copy(S_total))
+        push!(I, copy(I_total))
+        push!(R, copy(R_total))
+
+    return t, S, I , R
+
+
+#-----------------------------------------------------------------------------
+#=
+Inputs
+t       : Array of times at which events have occured
+SIR     : Array of arrays of Num people susceptible, infected and recovered at
+            each t
+N       : Population size
+
+Outputs
+png     : plot of SIR model over time [by default]
+=#
+function plots(t, SIR, N, Display=true, save=true)
+    gr(reuse=true)
+
+    plot(t, SIR, label=["Susceptible" "Infected" "Recovered"], lw = 2)
+    #plot!(t, I, label="Infected", show = true)
+    #display(plot!(t, R, label="Recovered", show = true))
+
+    xlabel!("Time")
+    ylabel!("Population Number")
+    title!("SIR model over time with a population size of $N")
+    plot!(size=(750,500))
+
+    if Display
+        # required to display graph on plots.
+        display(plot!())
+    end
+
+    if save
+        # Save graph as pngW
+        png("juliaGraphs/SIR_Model_Pop_$N")
+    end
+end
+
+#-----------------------------------------------------------------------------
+# testing the gillespieDirect2Processes function
+
+# Get same thing each time
+Random.seed!(1)
+
+# initialise variables
+N = [5, 10, 50, 100,1000,10000]
+
+S_total = N .- 1
+I_total = N .* 0 .+ 1
+R_total = N .* 0
+
+t_max = 200
+alpha = 0.4
+gamma = 0.0004
+
+# iterate through populations
+for i in 1:length(N)
+    t, S, I, R = gillespieDirect2Processes(t_max, S_total[i], I_total[i],
+        R_total[i], alpha, gamma, N)
+
+    plots(t, [S, I, R], N[i])
+
+end

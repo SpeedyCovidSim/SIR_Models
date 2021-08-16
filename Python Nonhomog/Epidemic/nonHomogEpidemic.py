2a29100a6ff875f4936ace120730bbd0158f8f45
nesaking100 <josh@looker.co.nz>
2021-08-16 16:50:55 +1200

    exp pipeline init

--- Python Nonhomog/Epidemic/nonHomogEpidemic.py
+++ Python Nonhomog/Epidemic/nonHomogEpidemic.py
@@ -1,8 +1,6 @@
 import numpy as np
 import igraph as ig
 from numpy import random
-from plots import plotSIR, plotSIRK
-import time
 
 def firstInverseMethod(updateFunction, numProcesses, timeLimit=100):
     '''
@@ -217,7 +215,7 @@ def nMGA(tMax, network, iTotal, sTotal, rTotal, numInfNei, susceptible, rateFunc
     I = I[:i]
     return t, S, I, R
 
-def gillespieNonHomogNetwork(tMax, network, iTotal, sTotal, rTotal, numInfNei, susceptible, maxalpha, maxbeta, rateFunction, tInit = 0.0):
+def gillespieMax(tMax, network, iTotal, sTotal, rTotal, numInfNei, susceptible, maxalpha, maxbeta, rateFunction, tInit = 0.0):
     '''
     Direct Gillespie Method, on network
     Uses numpy's random module for r.v. and sampling
@@ -312,101 +310,3 @@ def gillespieNonHomogNetwork(tMax, network, iTotal, sTotal, rTotal, numInfNei, s
     R = R[:i]
     I = I[:i]
     return t, S, I, R
-
-def setNetwork(network, prop_i=0.05):
-    '''
-    Initialises a parsed network with required random infected individuals (1 if too small)
-    Inputs
-    network     : igraph network object
-    prop_i      : proportion of population that is infected (default 0.05)
-    Outputs
-    iTotal      : number of infected
-    sTotal      : number of susceptible
-    rTotal      : number of recovered
-    numInfNei   : number of infected neighbour of each vertex
-    rates       : hazard rate of each vertex
-    susceptible : boolean array if vertex is susceptible or not
-    '''
-    # get random proportion of populace to be infected and set states
-    N = network.vcount()
-    numInfected = int(np.ceil(prop_i*N))
-    infecteds = list(random.choice(N, numInfected, replace=False))
-    susceptible = np.ones(N)
-    susceptible[infecteds] = 0
-
-    # set SIR numbers
-    iTotal = numInfected
-    sTotal = N-numInfected
-    rTotal = 0
-
-    # adding in hazards/rates
-    numInfNei = initHazards(network, infecteds, N)
-    return iTotal, sTotal, rTotal, numInfNei, susceptible
-
-def initHazards(network, infecteds, N):
-    '''
-    inits numInfNei array
-    Inputs
-    network     : igraph network object
-    infecteds   : array of infected vertices
-    N           : total number of vertices
-    Outputs
-    numSusNei   : number of susceptible neighbours for each vertex
-    '''
-    numInfNei = np.zeros(N)
-    # loop over all infected vertices
-    for inf_vert in network.vs(infecteds):
-        # Increase number of susceptible neighbours for neighbouring vertices
-        neighbours = network.neighbors(inf_vert)
-        for n in neighbours:
-            numInfNei[n] += 1
-    # don't count infecteds for speed-up
-    numInfNei[infecteds] = 0
-    return numInfNei
-
-def main():
-    '''
-    Main loop for testing within this Python file
-    '''
-    # testing the gillespieDirect2Processes function
-    # note random seed set within network model so result will occur everytime
-
-    # initialise variables
-    # N = np.array([5, 10, 50, 100,1000])
-    # k = [2,3,10,20,100]
-    N = 100
-  
-    tMax = 200
-    alpha = 0.4
-    beta = 10 / N
-    
-    def rate_function(susceptible, network, entry_times, sim_time, maxbeta, maxalpha):
-        rates = np.zeros(len(susceptible))
-        for i in range(len(susceptible)):
-            if (susceptible[i]==0):
-                rates[np.intersect1d(np.nonzero(susceptible==1),network.neighbors(i))] += max(maxbeta*1e-4,maxbeta/10*abs(sim_time-entry_times[i]-5)+maxbeta)
-                rates[i] = maxalpha
-        return rates
-
-
-    # iterate through populations for complete graphs
-    if True:
-        print("Beginning Ernos-Renyi simulations")
-        start = time.time()
-
-        for i in range(1):
-            print(f"Iteration {i} commencing")
-            network = ig.Graph.Erdos_Renyi(100,0.1)
-            iTotal, sTotal, rTotal, numInfNei, susceptible = setNetwork(network)
-            print(f"Beginning simulation {i}")
-            t, S, I, R = gillespieNonHomogNetwork(tMax, network, iTotal, sTotal, rTotal, numInfNei, susceptible, alpha, beta, rate_function)
-        end = time.time()
-        print(f"Avg. time taken for Lumped hazards simulation: {(end-start)/10}")
-
-        print(f"Exporting last simulation {i}")
-        # plot and export the simulation
-        outputFileName = f"pythonGraphs/nonMarkovSim/SIR_Model_Pop_{N}"
-        plotSIR(t, [S, I, R], alpha, beta, N, outputFileName, Display=False)
-
-if __name__=="__main__":
-    main()

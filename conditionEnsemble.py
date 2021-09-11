b1d4bfa6ffd062705111f1e5ff0eb37b4d690d20
Joel Trent <joeltrent1@gmail.com>
2021-09-12 09:49:28 +1200

    Updated to allow ABC conditioning of model outputs

--- conditionEnsemble.py
+++ conditionEnsemble.py
@@ -1,9 +1,12 @@
 import sys
+from numpy.core.function_base import linspace
 import pandas as pd
 sys.path.append("/Users/joeltrent/Documents/GitHub")
 sys.path.append("/Users/joeltrent/networkcontagion/conditioning")
 sys.path.append("/Users/joeltrent/networkcontagion/conditioning/lib")
 import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
 
 from ccandu.ccandu import conditional_composer as cc
 from plot_conditioned import make_conditioned_plot as mcp
@@ -14,16 +17,17 @@ import get_GP_model as ggm
 def condition(dir):
 
     net = False
-    daily = True
+    daily = False
     t_coeff = ggm.performTukeyapprox(dir, daily, net)
     print(f'transformation coefficient: {t_coeff}')
     output_dict = ggm.condition_linear_sample(dir, t_coeff, daily, net, full_return=True)
 
     # August 2021
-    # data = [1, 10, 20, 32, 51, 74, 107, 148, 210]#, 278, 348, 430, 513, 566, 615, 690, 739, 767, 787, 807, 827, 848, 863]
-    data = [1, 9, 10, 12, 19, 23, 33, 41, 62, 68, 70, 82, 83, 53, 49, 75, 49, 28, 20, 20, 20, 21, 15]
+    data = [1, 10, 20, 32, 51, 74, 107, 148, 210, 278, 348, 430, 513, 566, 615, 690, 739, 767, 787, 807, 827, 848, 863, 876]
+    # data = [1, 9, 10, 12, 19, 23, 33, 41, 62, 68, 70, 82, 83, 53, 49, 75, 49, 28, 20, 20, 20, 21, 15, 13]
 
     # data = [1,5,9,25,45,72,108,145,205,274,353,429,508,562,627,686,731,759]
+    # data = [1,4,4,16,20,27,36,37,62,66,79,76,79,54,62,59,45,28,22,24,15,26,13,14]
 
     # August 2020
     # data = [1, 4, 17, 29, 34, 47, 59, 68, 78, 83, 85, 92, 94, 101]
@@ -39,15 +43,79 @@ def condition(dir):
         cond_runs = mcp(output_dict, data, net, band = 'quant', net_mod = 'filter', error=False, Reff=False, save=False)
         return cond_runs
 
-        
+def conditionABC(dir):
+
+    net=False
+    input_data = pd.read_csv(f"{dir}/parameters.csv", index_col=0)
+    config = input_data.iloc[0]
+    i = config.name
+
+    # conf_cases, cumul_cases, detected, df = get_cases(i, dir, net)
+
+    conf_cases, cumul_cases, detected, df = ggm.get_cases(i, dir, net)
+
+    data = np.array([[1, 10, 20, 32, 51, 74, 107, 148, 210, 278, 348, 430, 513, 566, 615, 690, 739, 767, 787, 807, 827, 848, 863, 876]])
+
+    filtered_ABC = cc.filter_ABC(realisations=conf_cases, obs_indices=list(range(0, len(data[0]))), 
+        data=data, q_tol=0.01, return_indices=False)
+
+    indexes = cc.filter_ABC(realisations=conf_cases, obs_indices=list(range(0, len(data[0]))), 
+        data=data, q_tol=0.01, return_indices=True)      
+
+    t = list(range(0, len(data[0])))
+
+    sns.set()
+    sns.set_style('ticks')
+    sns.set_color_codes('pastel')
+    f = plt.figure(figsize=[6,5],dpi=300)
+    # palette = ['#A1C9F4', '#FF9F9B', '#8DE5A1', '#B9F2F0']
+    # palette = ['#A1C9F4', '#FF9F9B','#8DE5A1', '#B9F2F0']
+
+
+    for i in range(0, len(indexes[1])):
+        plt.plot(list(range(0, len(filtered_ABC[i,:]))), filtered_ABC[i,:], "b-", lw=1, alpha = 1.0)
+
+    # for i in indexes[1]:
+    #     plt.plot(list(range(0, len(conf_cases[i]))), conf_cases[i], "r*", lw=1, alpha = 0.2)
+
+
+
+    plt.plot(t, data[0], "k-", label="data", lw=2, alpha = 1.0)
+    # else
+    # # end
+    # plt.xlabel("Reff")
+    # plt.ylabel("Probability")
+
+    # plt.title(title)
+    plt.legend(loc = 'lower right')
+
+    Display=True
+    if Display:
+        # required to display graph on plots.
+        plt.show()
+
+    # if save:
+    #     # Save graph as pngW
+    #     plt.savefig(outputFileName)
+    indexesJulia = np.array(indexes[1]) + 1
+    return indexesJulia
+
 
 dir = '/Users/joeltrent/Documents/GitHub/SIR_Models/August2021Outbreak/CSVOutputs'
 # dir = '/Users/joeltrent/Documents/GitHub/SIR_Models/August2020OutbreakFit/CSVOutputs'
 # print()
 
-DF = pd.DataFrame(condition(dir))
+# DF = pd.DataFrame(condition(dir))
 
-# save the dataframe as a csv file
-DF.to_csv('/Users/joeltrent/Documents/GitHub/SIR_Models/August2021Outbreak/CSVOutputs/indexes_ensemble_config_8_8Sep.csv', header=False, index=False)
+# # save the dataframe as a csv file
+# DF.to_csv('/Users/joeltrent/Documents/GitHub/SIR_Models/August2021Outbreak/CSVOutputs/indexes_ensemble_config_7_10Sep_cumulative.csv', header=False, index=False)
 
 # DF.to_csv('/Users/joeltrent/Documents/GitHub/SIR_Models/August2020OutbreakFit/CSVOutputs/indexes_ensemble_6Sep_MatchMike.csv', header=False, index=False)
+
+
+dir = '/Users/joeltrent/Documents/GitHub/SIR_Models/August2021Outbreak/CSVOutputs'
+
+DF = pd.DataFrame(conditionABC(dir))
+DF.to_csv('/Users/joeltrent/Documents/GitHub/SIR_Models/August2021Outbreak/CSVOutputs/indexes_ensemble_config_11_11Sep_ABC.csv', header=False, index=False)
+
+
